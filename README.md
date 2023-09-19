dd8b4ebbb198c94e46b6ba80671dd7dd86355c45
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-09-18 23:53:50 -0700

    program three updated and fully fuctional as described

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Python List and Dictionary Program
 Program Password: admin
 
 Description:
-In this assignment, I use a class/dictionary Python data structure, included a list, and use a menu to guide a user to access list info. The user ultimately can view or add their contact info to a list holding a temporary class/dictionary structure of contacts. Each contact has a name, phone number, email address and is referenced by the name "card" or "contact card". Code sections are labled with comments to help the reader understand my code. The user menu relies heavily on semi-accurate user input. Users can make typos and the program will try to validate on implied inference of user intended input. The program askes for decision confirmation within the flow of code and menu traversal is relativley easy to follow bu is very linear. User menu options to navigate menus in a flow of decision input which allow the user to go back and forth are smooth linear menu paths. The user also has options depending on the input scenarios to terminate or restart the program. Due to the limited functionality of the written code program functionality has been limited to a user viewing, or adding, or can reset the contact cards and list by ending the program to allow the list to reset. Card data is reset to the original list provided in class assingment parameters.
+In this assignment, I use a class/dictionary Python data structure, included a list, and use a menu to guide a user to access list info. The user ultimately can view, search, or add their contact info to a list holding a temporary class/dictionary structure of contacts. Each contact has a name, phone number, email address and is referenced by the name "card" or "contact card". Code sections are labled with comments to help the reader understand my code. The user menu relies heavily on semi-accurate user input. Users can make typos and the program will try to validate on implied inference of user intended input. The program askes for decision confirmation within the flow of code and menu traversal is relativley easy to follow but is very linear. User menu options will navigate menus in a flow of decision input which allow the user to go back and forth as smoothly as possible in a linear menu paths. The user also has options depending on the input scenarios to terminate or restart the program. Due to the limited functionality of the written code program functionality has been limited to a user viewing, or searching, or adding, or can reset the contact cards and list by ending the program to allow the list to reset. Card data is reset to the original list provided in class assingment parameters.
 
 Delivered:
 
@@ -26,53 +26,38 @@ ReadMe must be read in order to gain admin access to the list on GitHub Classroo
 
 I created a demo program to show how a dictionary/class can be used to create a list and include user input.
 
-Comments: My program is more focused on a smooth and linear user menu interface. User input and validation allows for a guided path to view, add, review, and terminate an augmentable temporary list of contacts. The code itself should be scalable with minimal adjustment to read and write from an existing text file/list.
+Comments: My program is more focused on a smooth and linear user menu interface. User input and validation allows for a guided path to view,search, add, review, and terminate an augmentable temporary list of contacts. The code itself should be scalable with minimal adjustment to read and write from an existing text file/list.
 
-Testing Results: Testing program flow and pathways resulted in all scenarios tested to be succesful with no runtime, logic, or syntax errors. All user input results in the program accepting it or requesting the user to re-enter input or return to a previous menu.
+Testing Results: Testing program flow and pathways resulted in all scenarios tested to be succesful with no runtime, logic, or syntax errors. All user input results in the program accepting it or requesting the user to re-enter input or return to a previous menu. The code was filtered through chatgpt which suggested to remove most of my functions for simple boolean which reduced code lines however this would have my code less readable. My functions just make it easier to know whats going on. ChatGPT did confirm my code functions as intended. It is my code and I have old code iterations to show my logical flow of adjustments I made.  
 
 Requirements:
 
-Create an empty list called contacts to store contact information. Each contact should be represented as a dictionary with the following keys:
+Created an empty list called contacts to store contact information. Each contact should be represented as a dictionary with the following keys:
 
 'name': The name of the contact (a string).
 'phone': The phone number of the contact (a string).
 'email': The email address of the contact (a string).
 
-Implement a function called add_contact() that takes user input to add a new contact to the contacts list. Ensure that each contact has a unique name.
+Implemented a function called to add contacts that takes user input to add a new contact to the contacts list.
 
-Implement a function called view_contacts() that displays the list of all contacts in a user-friendly format.
+Implemented a function to view contacts that displays the list of all contacts in a user-friendly format.
 
-Implement a function called search_contact() that takes a name as input and searches for the contact by name. If found, display the contact's details. If not found, display a message indicating that the contact was not found.
+Implemented a function that searches contacts that takes a name as input and searches for the contact by name. If found, display the contact's details. If not found, display a message indicating that the contact was not found.
 
-Use string formatting and string methods (e.g., format(), split(), strip(), etc.) to format and validate user input as necessary.
+Used formatting and string methods (e.g., format(), split(), strip(), etc.) to format and validate user input as necessary.
 
 Additional Guidelines:
 
 Basic: Make sure to create user-defined methods for adding, viewing, and searching for contacts.
 
-Basic: Commit to your GitHub Classroom repo whenever you create a program update. This shows me that you're working and learning, and also earns you commits on your GitHub profile!
+Basic: Committed to GitHub Classroom repo whenever updated.
 
-Advanced: Your program will provide a menu with options to add a contact, view all contacts, search for a contact, and exit the program.
+Advanced: Program will provide a menu with options to add a contact, view all contacts, search for a contact, and exit the program.
 
 Advanced: Implement error handling to handle invalid user input.
-
-Advanced: Use Python file i/o to output your list of contcts to a text file on your local machine. 
 
 Submission:
 
 Submit your "python_program_three.py" program and update the ReadMe.md file in your GitHub Classroom remote repository by the specified deadline.
-
-Grading Criteria:
-
-You will be assessed based on the following criteria:
-
-Correctness of the program's functionality.
-Proper use of Python data structures (lists, dictionaries, strings).
-Clarity and organization of your code.
-Comprehensive and well-structured summary of your program in the ReadMe.md file.
-Adherence to Python coding conventions and best practices.
-
-Good luck, and enjoy working on this assignment! If you have any questions or need clarification, reach out during weekend office hours or via the Canvas inBox.
-
 
 
--- python_program_three.py
+++ python_program_three.py
@@ -35,8 +35,8 @@ def script():
                     exit(1)
         #view contacts
         if manager[0] == "Y":
+
             def admin_menu():
-                #global user_search
                 password = input("Admin Menu --- Please enter password: ").lower()
                 if password == "admin":
                     print("\nThank You, Admin. Menu will move to contact cards.\nThe following shows contact card "
@@ -49,8 +49,14 @@ def script():
                     print("\nMac Mac's program will test user accessibility.\nHere is Mac Mac's card: \n")
                     card_mac_mac = {"name": "Mac Mac", "phone": "559-365-8702", "email": "bongo90g@yahoo.com"}
                     print(card_mac_mac)
-                    print("\nAdding Mac Mac's card to the list... \n")
-                    contacts.append(card_mac_mac)
+
+                    # Adding Mac Mac's card to the list...
+                    mac_mac_exists = any(card == card_mac_mac for card in contacts)
+                    if not mac_mac_exists:
+                        contacts.append(card_mac_mac)
+                    else:
+                        print("\nMac Mac's card already exists in the list. Not adding a duplicate.")
+
                     print("\n")
                     print(type(contacts))
                     print(contacts)
@@ -61,32 +67,37 @@ def script():
                     def manage():
                         # search function
                         def user_search():
-                            search_input = input("\nSearch by Name: ")
+                            print("\n")
+                            print(contacts)
+                            search_input = input(
+                                "\nSearch by Name: ").lower()  # Convert input to lowercase for case-insensitive search
                             print('\nYou are searching for "' + search_input + '".')
+                            found_cards = []
+
                             for card in contacts:
-                                for key in card:
-                                    if card[key] == search_input:
-                                        search_result = card[key]
-                                        print("\nYour search query found a card for " + search_input)
-                                        print(search_result)
-                                        search_redo1 = input('\nDo you have another card name you wish to search or return to user menu? "Ok"/"EXIt": ').upper()
-                                        if search_redo1[0] == "O":
-                                            user_search()
-                                        else:
-                                            print("Returning to user menu.")
-                                            user_menu()
-                                    else:
-                                        print("Your query input has no results.")
-                                        search_redo2 = input('\nDo you have another card name you wish to search, '
-                                                             'return to admin manage, or to user menu? '
-                                                             '"Ok"/"return"/"EXIt": ').upper()
-                                        if search_redo2[0] == "O":
-                                            user_search()
-                                        if search_redo2[0] == "r":
-                                            manage()
-                                        else:
-                                            print("Returning to user menu.")
-                                            user_menu()
+                                if isinstance(card['name'], list):
+                                    # Join the elements of the list and convert to lowercase for comparison
+                                    card_name = ' '.join(card['name']).lower()
+                                else:
+                                    card_name = card['name'].lower()
+
+                                if search_input in card_name:  # Partial match check
+                                    found_cards.append(card)
+
+                            if found_cards:
+                                print("\nYour search query found the following card(s):")
+                                for card in found_cards:
+                                    print(card)
+                            else:
+                                print("\nNo matching cards found.")
+
+                            search_redo1 = input('\nDo you have another card name you wish to search or '
+                                                 'return to manage menu? "Ok"/"EXIt": ').lower()
+                            if search_redo1.startswith("o"):
+                                user_search()
+                            else:
+                                print("\nReturning to manage menu.\n")
+                                manage()
                         # user_input function
                         def user_input():
                             print("\nExcellent. Please follow prompts.")
@@ -113,7 +124,7 @@ def script():
                         manage_input = input("\nChoose Admin task from the following menu. " + 'Enter "1" to perform '
                                                                                               'individual card '
                                                                                               'search. / Enter "2" to '
-                                                                                              'provide input. /Enter '
+                                                                                              'provide input. / Enter '
                                                                                               '"0" to cancel admin '
                                                                                               'access: ')
                         if manage_input == "1":
@@ -132,13 +143,12 @@ def script():
                             else:
                                 print('Program end')
                                 exit(1)
-                        if manage_input != "1" or "2":
-                            print("Admin Command was not understood. Please type new command: ")
-                            manage()
-
-                        else:
+                        if manage_input == "0":
                             print("\nAdmin access cancelled.")
                             admin_menu()
+                        if manage_input != "1" or "2" or "0":
+                            print("Admin Command was not understood. Please type new command: ")
+                            manage()
                     manage()
                 else:
                     print("Password input was not understood.\n")
@@ -154,3 +164,121 @@ def script():
             user_menu()
     user_menu()
 script()
+
+"""
+                    def manage():
+                        # search function
+                        def user_search():
+                            print("\n")
+                            print(contacts)
+                            search_input = input(
+                                "\nSearch by Name: ").lower()  # Convert input to lowercase for case-insensitive search
+                            print('\nYou are searching for "' + search_input + '".')
+
+                            found_cards = []
+
+                            for card in contacts:
+                                for key in card:
+                                    # Convert the card's name to lowercase for case-insensitive comparison
+                                    card_name = card['name'].lower()
+                                    if search_input in card_name:  # Partial match check
+                                        found_cards.append(card)
+
+                            if found_cards:
+                                print("\nYour search query found the following card(s):")
+                                for card in found_cards:
+                                    print(card)
+                            else:
+                                print("\nNo matching cards found.")
+
+                            search_redo1 = input('\nDo you have another card name you wish to search or '
+                                                 'return to user menu? "Ok"/"EXIt": ').lower()
+                            if search_redo1.startswith("o"):
+                                user_search()
+                            else:
+                                print("\nReturning to user menu.\n")
+                                user_menu()
+                            if card['name'] != search_input:
+                                        print("\nYour query input has no results.")
+                                        search_redo2 = input('\nDo you have another card name you wish to search, '
+                                                             'return to admin manage, or to user menu? '
+                                                             '"Ok"/"return"/"EXIt": ').upper()
+                                        if search_redo2[0] == "O":
+                                            user_search()
+                                        if search_redo2[0] == "r":
+                                            manage()
+                                        else:
+                                            print("Returning to user menu.")
+                                            user_menu()
+
+                        # search function
+                        def user_search():
+                            print("\n")
+                            print(contacts)
+                            search_input = input("\nSearch by Name: ")
+                            print('\nYou are searching for "' + search_input + '".')
+                            for card in contacts:
+                                for key in card:
+                                    print(card[key])
+                                    print(card[key] == search_input)
+                                    while card['name'] == search_input:
+                                        search_result = card
+                                        print("\nYour search query found a card for " + search_input)
+                                        print(search_result)
+                                        search_redo1 = input('\nDo you have another card name you wish to search or '
+                                                             'return to user menu? "Ok"/"EXIt": ').upper()
+                                        if search_redo1[0] == "O":
+                                            user_search()
+                                        else:
+                                            print("Returning to user menu.")
+                                            user_menu()
+            def admin_menu():
+                password = input("Admin Menu --- Please enter password: ").lower()
+                if password == "admin":
+                    print("\nThank You, Admin. Menu will move to contact cards.\nThe following shows contact card "
+                          "information:\n")
+                    print(type(card))
+                    for key, value in user_card.items():
+                        print(f"{key}: {value}")
+                    print("\nThe following shows the list of current contact cards:\n\n")
+                    print(contacts)
+                    print("\nMac Mac's program will test user accessibility.\nHere is Mac Mac's card: \n")
+                    card_mac_mac = {"name": "Mac Mac", "phone": "559-365-8702", "email": "bongo90g@yahoo.com"}
+                    print(card_mac_mac)
+                    print("\nAdding Mac Mac's card to the list... \n")
+                    contacts.append(card_mac_mac)
+                    print("\n")
+                    print(type(contacts))
+                    print(contacts)
+                    print("\n")
+                    print("****Disclaimer****\nMac Mac's List and Dictionary Program is in limited alpha demo phase. "
+                          "This is not a permanent directory. Please input your contact card info in the following "
+                          "prompt.")
+
+def admin_menu():
+            # global user_search
+            password = input("Admin Menu --- Please enter password: ").lower()
+            if password == "admin":
+                print("\nThank You, Admin. Menu will move to contact cards.\nThe following shows contact card "
+                      "information:\n")
+                print(type(card))
+                for key, value in user_card.items():
+                    print(f"{key}: {value}")
+                print("\nThe following shows the list of current contact cards:\n\n")
+                print(contacts)
+                for card in contacts:
+                    for key in card:
+                        if card[key] != "Mac Mac":
+                            print("\nMac Mac's program will test user accessibility.\nHere is Mac Mac's card: \n")
+                            card_mac_mac = {"name": "Mac Mac", "phone": "559-365-8702", "email": "bongo90g@yahoo.com"}
+                            print(card_mac_mac)
+                            print("\nAdding Mac Mac's card to the list... \n")
+                            contacts.append(card_mac_mac)
+                print("\n")
+                print(type(contacts))
+                print(contacts)
+                print("\n")
+                print("****Disclaimer****\nMac Mac's List and Dictionary Program is in limited alpha demo phase. "
+                      "This is not a permanent directory. Please input your contact card info in the following "
+                      "prompt.")
+                      """
